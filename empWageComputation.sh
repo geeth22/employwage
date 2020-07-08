40754433823a75a9228134a992887b2b74e4e28e
geeth22 <geethchandra22@gmail.com>
2020-07-08 15:27:01 +0530

    update commit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,16 @@
 #!/bin/bash -x
 
-res=$(( RANDOM % 3 ))  # generating random number 0,1,2
-
 #CONSTANTS
 WagePerHour=20
 FullDayHour=8
 PartTimeHour=4
 
-#VARIABLE
-DailyWage=0
+res=$(( RANDOM % 3 ))  # generating random number 0,1,2
 
-if [[ $res -eq 2 ]] ; then
-	DailyWage=$(($WagePerHour*$FullDayHour))
-	echo "dailywage = $DailyWage"
-elif [[ $res -eq 1 ]] ; then
-	DailyWage=$(($WagePerHour*$PartTimeHour))
-	echo "dailywage = $DailyWage"
-else
-	echo " dailywage = $DailyWage"
-fi
+case $res in
+	1)WorkHours=4 ;;
+	2)WorkHour=8 ;;
+	*)WorkHour=0 ;;
+esac
+
+DailyWage=$(($WagePerHour*$WorkHour))
