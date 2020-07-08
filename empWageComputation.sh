1863aefafe35f6c3702d7a570d5a21e94a74df7d
geeth22 <geethchandra22@gmail.com>
2020-07-08 15:08:23 +0530

    update commit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,6 @@
-Welcome to Employee Wage Computation Program
+#!/bin/bash -x
+
+echo "Welcome to Employee Wage Computation Program"
 
 result=$(( RANDOM % 2 ))  # generating random number 0 or 1
 
@@ -6,4 +8,18 @@ if [[ $result -eq 0 ]] ; then
 	echo "absent"
 else 
 	echo "present"
+fi
+
+#CONSTANTS
+WagePerHour=20
+FullDayHour=8
+
+#VARIABLE
+DailyWage=0
+
+if [[ $result -eq 0 ]] ; then
+	echo "dailywage = $DailyWage"
+else
+	DailyWage=$(($WagePerHour*$FullDayHour))
+	echo "dailywage = $DailyWage"
 fi
