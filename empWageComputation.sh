ef566d0c8d12b17eca935fe4f2e667561b004283
geeth22 <geethchandra22@gmail.com>
2020-07-08 15:01:34 +0530

    update commit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,9 @@
 Welcome to Employee Wage Computation Program
+
+result=$(( RANDOM % 2 ))  # generating random number 0 or 1
+
+if [[ $result -eq 0 ]] ; then 
+	echo "absent"
+else 
+	echo "present"
+fi
