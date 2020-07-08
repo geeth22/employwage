9eb1bee2de839b8aca981b92308ecd6b3a33ab98
geeth22 <geethchandra22@gmail.com>
2020-07-08 15:44:31 +0530

    update commit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,14 +5,23 @@ WagePerHour=20
 FullDayHour=8
 PartTimeHour=4
 WorkingDaysPerMonth=20
+TotalWorkingHoursPerMonth=100
 
-res=$(( RANDOM % 3 ))  # generating random number 0,1,2
+#VARIABLES
+TotalWorkingDays=0
+TotalWorkHours=0
 
-case $res in
-	1)WorkHours=4 ;;
-	2)WorkHour=8 ;;
-	*)WorkHour=0 ;;
-esac
+while [[ $TotalWorkHours -lt $TotalWorkingHoursPerMonth && $TotalWorkingDays -lt $WorkingDaysPerMonth ]]
+do
+	((TotalWorkingDays++)
+	res=$(( RANDOM % 3 ))  # generating random number 0,1,2
+	case $res in
+		1)WorkHours=4 ;;
+		2)WorkHours=8 ;;
+		*)WorkHours=0 ;;
+	esac
+	echo $WorkHours
+	TotalWorkHours=$(($TotalWorkHours+$WorkHours))
+done
 
-DailyWage=$(($WagePerHour*$WorkHour))
-TotalMonthWage=$(($WorkingDaysPerMonth*$DailyWage))
+TotalMonthWage=$(($TotalWorkHours*$WagePerHour))
