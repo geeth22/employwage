bb1367c15c18aea1364977fcf2007161192b2617
Geeth <66339384+geeth22@users.noreply.github.com>
2020-07-09 14:48:58 +0530

    Update empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,4 @@
-#!/bin/bash -x
-
-echo "Welcome to Employee Wage Computation Program"
-
-result=$(( RANDOM % 2 ))  # generating random number 0 or 1
-
-if [[ $result -eq 0 ]] ; then 
-	echo "absent"
-else 
-	echo "present"
-fi
+#usecase 2
 
 #CONSTANTS
 WagePerHour=20
